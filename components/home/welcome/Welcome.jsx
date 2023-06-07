e8df7b934a57dfc9101c2e7b1b0d243e37e0918c
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-06-08 01:46:20 +0530

    Creating Home Messaeg

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+
+import { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native'
+
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons , SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Ishaan...</Text>
+        <Text style={styles.welcomeMessage}>What a lovely weather to find you a perfect job!</Text>
+      </View>
+
     </View>
   )
 }
