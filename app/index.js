0b733ac644c87cafda93d520c62475020b5b4cf0
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-06-08 00:50:16 +0530

    Creating Home Page

--- app/index.js
+++ app/index.js
@@ -1,13 +1,46 @@
-import { View } from "react-native";
-import { Link, Stack } from "expo-router";
+import { useState } from "react";
+import { View , ScrollView , SafeAreaView } from "react-native";
+import { Link, Stack , useRouter } from "expo-router";
+
+import {COLORS , icons , images , SIZES } from '../constants'
+import { Nearbyjobs , Popularjobs , ScreenHeaderBtn , Welcome } from '../components'
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      {/* Use the `Screen` component to configure the layout. */}
-      <Stack.Screen options={{ title: "Overview" }} />
-      {/* Use the `Link` component to enable optimized client-side routing. */}
-      <Link href="/details">Go to Details</Link>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+
+      <Stack.Screen options= 
+        {{ 
+        	headerStyle: { backgroundColor: COLORS.lightWhite } ,
+			headerShadowVisible: false,
+			headerLeft: () => (
+					<ScreenHeaderBtn iconUrl = {icons.menu} dimentions = "60%">
+
+					</ScreenHeaderBtn>
+				)
+			 , 
+			headerRight: () => (
+					<ScreenHeaderBtn iconUrl = {icons.profile} dimentions = "100%">
+
+					</ScreenHeaderBtn>
+				)
+			,
+			headerTitle: ""
+        }} 
+    	/>
+
+		<ScrollView showsVerticalScrollIndicator={false}>
+			<View 
+				style={{
+					flex: 1,
+					padding: SIZES.medium
+				}}
+			>
+				<Welcome/>
+				<Popularjobs />
+				<Nearbyjobs />
+			</View>
+		</ScrollView>
+    </SafeAreaView>
   );
 }
